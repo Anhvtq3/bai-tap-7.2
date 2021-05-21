8b66cc25a18fec469a5f7c3c4cd012d0e44bde64
Anhvtq3 <83685681+Anhvtq3@users.noreply.github.com>
2021-05-21 21:49:55 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,105 +1,65 @@
-import jdk.internal.dynalink.beans.StaticClass;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) {
-//        StringArr();
-      StringArr1();
+    public static void main(String args[]){
+    stringArray();
+
     }
-
-    static void StringArr() {
-        int temp;
+    static void stringArray(){
+        //Mang String
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập số phần tử của mảng: ");
-        int size = sc.nextInt();
-        int strArr[] = new int[size];
-        for (int i = 0; i < strArr.length; i++) {
-            System.out.printf("Nhập phần tử thứ %d: ", i);
-            strArr[i] = sc.nextInt();
+        System.out.println("Nhập số phần tủ cho mang: ");
+        int sum=0;
+        int sum1=0;
+        int temp;
+        int size =sc.nextInt();
+        int strArray[]= new int[size];
+        for (int i=0; i<strArray.length;i++){
+            System.out.println("Nhập phần tử ở ví trí:" +i);
+            strArray[i]=sc.nextInt();
         }
-        show(strArr);
-        //Tìm số lớn nhất trong mảng và index của phần tử đó
-        int maxvalue = strArr[0];
-        int index1 = 0;
-        for (int i = 1; i < strArr.length; i++) {
-            if (strArr[i] > maxvalue) {
-                maxvalue = strArr[i];
-                index1 = i;
+//        System.out.println("Đây là mảng");
+//        for (int i=0;i<strArray.length;i++){
+//            System.out.println(strArray[i]);
+//            show(size);
+//        }
+        show(strArray);
+        System.out.println("Phần tủ ở vị trí index số 2 là: "+strArray[2]);
+        for (int i=0;i<strArray.length;i++){
+            sum=sum+strArray[i];
+        }
+        System.out.println("Tổng các phần tủ trong mảng là: "+sum);
+        for(int i=0;i<strArray.length;i++){
+            if (strArray[i]%2==0){
+                System.out.println("Các số chẵn trong mảng: "+ i);
+                sum1=sum1+strArray[i];
             }
         }
-
-        System.out.println("Giá trị lớn nhất của mảng là: " + maxvalue + " và index của phần tử này là: " + index1);
-
-        //tìm số lớn thứ 2 trong mảng
-        int max1 = strArr[0];
-        int max2 = strArr[0];
-        for (int i = 1; i < strArr.length; i++) {
-            if (strArr[i] > max1) {
-                max1 = strArr[i];
-            }
-            for (int j = 1; j < strArr.length; j++) {
-                if ((strArr[j] > max2) && (strArr[j] < max1)) {
-                    max2 = strArr[j];
+            System.out.println("Tổng các số chẵn trong mảng= "+ sum1);
+        for(int i=0; i<strArray.length;i++){
+            for(int j=i+1;j<strArray.length;j++){
+                if (strArray[i]>strArray[j]){
+                    temp=strArray[i];
+                    strArray[i]=strArray[j];
+                    strArray[j]=temp;
                 }
             }
         }
-
-        System.out.println("Giá trị lớn thứ 2 trong mảng: " + max2);
-        //Tìm số bé nhất trong mảng
-        int minvalue = strArr[0];
-        int index2 = 0;
-        for (int i = 1; i < strArr.length; i++) {
-            if (strArr[i] < minvalue) {
-                minvalue = strArr[i];
-                index2 = i;
-            }
-        }
-        System.out.println("Giá trị nhỏ nhất trong mảng là : " + minvalue + " và index của phần tử trong mảng là: " + index2);
-
-    }
-
-    //Bài tập thứ 2
-    static void StringArr1() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập số phần tử của mảng: ");
-        int size = sc.nextInt();
-
-        String strArr1[] = new String[size];
-        for (int i = 0; i < strArr1.length; i++) {
-            System.out.printf("\nNhập phần tử thứ %d ", i);
-            strArr1[i] = sc.next();
-
-        }
-        show1(strArr1);
-        String version1="java";
-
-        int count=0;
-        for (int i = 0; i < strArr1.length; i++){
-            if(strArr1[i].toLowerCase().contains(version1)){
-                count+=1;
-            }
-
-        }
-        System.out.println("Số lần xuất hiện java= "+count);
-    }
-
-    public static void show(int[] arr) {
+        System.out.println("Sắp xếp lại mảng như sau:");
         System.out.println("Đây là mảng");
-
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int i=0;i<strArray.length;i++){
+            System.out.println(strArray[i]);
         }
-    }
+}
 
-    public static void show1(String[] arr) {
-        System.out.println("Đây là mảng");
+public static void show(int [] arr){
+    System.out.println("Đây là mảng");
 
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+    for (int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
     }
 }
+
+    }
 
