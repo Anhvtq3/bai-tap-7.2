e61b3f457f43c7becd9e744153bebde3f3ef1274
Anhvtq3 <83685681+Anhvtq3@users.noreply.github.com>
2021-05-21 21:54:42 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,65 +1,86 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-    stringArray();
-
+bai1();
     }
-    static void stringArray(){
-        //Mang String
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập số phần tủ cho mang: ");
-        int sum=0;
-        int sum1=0;
-        int temp;
-        int size =sc.nextInt();
-        int strArray[]= new int[size];
-        for (int i=0; i<strArray.length;i++){
-            System.out.println("Nhập phần tử ở ví trí:" +i);
-            strArray[i]=sc.nextInt();
-        }
-//        System.out.println("Đây là mảng");
-//        for (int i=0;i<strArray.length;i++){
-//            System.out.println(strArray[i]);
-//            show(size);
-//        }
-        show(strArray);
-        System.out.println("Phần tủ ở vị trí index số 2 là: "+strArray[2]);
-        for (int i=0;i<strArray.length;i++){
-            sum=sum+strArray[i];
-        }
-        System.out.println("Tổng các phần tủ trong mảng là: "+sum);
-        for(int i=0;i<strArray.length;i++){
-            if (strArray[i]%2==0){
-                System.out.println("Các số chẵn trong mảng: "+ i);
-                sum1=sum1+strArray[i];
+    static void bai1(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số phần tử của hàng: ");
+        int row=scanner.nextInt();
+        System.out.print("Nhập số phần tử của cột: ");
+        int cloum=scanner.nextInt();
+
+        int [][] A= new int[row][cloum];
+        int [][] B = new int[row][cloum];
+        for(int i=0;i<row;i++){
+            for (int j=0; j<cloum;j++){
+                System.out.printf("Nhập số phần tử mảng A ở ví trí  A[%d][%d]:", i, j);
+                A[i][j]= scanner.nextInt();
+
             }
         }
-            System.out.println("Tổng các số chẵn trong mảng= "+ sum1);
-        for(int i=0; i<strArray.length;i++){
-            for(int j=i+1;j<strArray.length;j++){
-                if (strArray[i]>strArray[j]){
-                    temp=strArray[i];
-                    strArray[i]=strArray[j];
-                    strArray[j]=temp;
-                }
+        show(A);
+        for(int i=0;i<row;i++){
+            for (int j=0; j<cloum;j++){
+                System.out.printf("Nhập số phần tử Mảng B ở ví trí  B[%d][%d]:", i, j);
+                B[i][j]= scanner.nextInt();
+
             }
         }
-        System.out.println("Sắp xếp lại mảng như sau:");
-        System.out.println("Đây là mảng");
-        for (int i=0;i<strArray.length;i++){
-            System.out.println(strArray[i]);
+        show(B);
+
+        int [][] C = new int[row][cloum];
+        for(int i=0;i<row;i++){
+            for (int j=0; j<cloum;j++){
+                C[i][j]=A[i][j]+B[i][j];
+                System.out.printf("Phần tử mảng C= A+B ở ví trị C[%d][%d]: %d",i,j,C[i][j]);
+            }
         }
-}
+        show(C);
+        int [][] chuyenviA = new int[cloum][row];
+        for (int i=0;i<row;i++){
+            for(int j=0;j<cloum;j++){
+                chuyenviA[j][i]=A[i][j];
+                System.out.printf(" chuyenviA[%d][%d]: %d",i,j,chuyenviA[i][j]);
+            }
+        }
+        show(chuyenviA);
+        int [][] chuyenviB = new int[cloum][row];
+        for (int i=0;i<row;i++){
+            for(int j=0;j<cloum;j++){
+                chuyenviB[j][i]=B[i][j];
+                System.out.printf("chuyenviB[%d][%d]: %d",i,j,chuyenviB[i][j]);
+            }
+        }
+        show(chuyenviB);
+    }
+    static void bai2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số phần tử của hàng: ");
+        int row = scanner.nextInt();
+        System.out.print("Nhập số phần tử của cột: ");
+        int cloum = scanner.nextInt();
 
-public static void show(int [] arr){
-    System.out.println("Đây là mảng");
+        int[][] AB = new int[row][cloum];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < cloum; j++) {
+                System.out.printf("Nhập số phần tử mảng A ở ví trí  A[%d][%d]:", i, j);
+                AB[i][j] = scanner.nextInt();
 
+            }
+        }
+    }
+    static void show(int[][] matran){
+        System.out.println("Hiển thị ma trận vừa nhập: ");
 
-    for (int i=0;i<arr.length;i++){
-        System.out.println(arr[i]);
+        for(int i=0;i<matran.length;i++){
+            for (int j=0; j<matran[i].length;j++){
+                System.out.print(matran[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
     }
 }
-
-    }
-
